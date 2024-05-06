940988e60ee93e3a429368b30919eceb5096e4c7
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-05-06 19:54:40 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Mail;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program is starting");
+
+        string username = "";       //set you google username (corporate)
+        string pass = "";           //set you password from gamil
+        string subject = "";        //set your mail title
+        string body = "";           //set your mail body
+
+        string[] addresses = File.ReadAllLines("");
+        SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+        client.EnableSsl = true;
+        client.Credentials = new NetworkCredential(username, pass);
+
+        int emailCount = 0;
+        List<string> sentAddresses = new List<string>();   //using for sent multiple addresses
+        Console.Write($"Emails sent: {emailCount}");
+        Console.WriteLine();
+
+        foreach (string address in addresses)
+        {
+            MailMessage message = new MailMessage(username, address, subject, body);
+            try
+            {
+                client.Send(message);
+                emailCount++;
+                sentAddresses.Add(address);
+                Console.SetCursorPosition(0, 0);
+                Console.Write($"Emails sent to: {emailCount}; ");
+                Console.WriteLine("");
+                foreach(string sentAddress in sentAddresses)
+                {
+                    Console.WriteLine($" - {sentAddress}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending mail to " + address + ": " + ex.Message);
+            }
+
+            Thread.Sleep(5000);
+        }
+    }
+}
